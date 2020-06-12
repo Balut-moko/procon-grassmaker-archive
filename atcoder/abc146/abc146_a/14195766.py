415065f5b52b48d16d62815affc6d501dd04c2c7
Balut <hiro.account@gmail.com>
2020-06-12 20:29:00 +0900

    abc146_a

---
+++ atcoder/abc146/abc146_a/14195766.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    S = input()
+    week = ['SUN','MON','TUE','WED','THU','FRI','SAT']
+    print(7-week.index(S))
+    
+if __name__ == "__main__":
+    main()
