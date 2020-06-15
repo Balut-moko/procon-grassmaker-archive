ee233c8d11fbf402673a2171137879e3c851c170
Balut <hiro.account@gmail.com>
2020-06-15 13:54:49 +0900

    APG4b_cm

---
+++ atcoder/APG4b/APG4b_cm/14380449.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  // ここにプログラムを追記
+  int i;
+  cout << "A:";
+  while(i<A){
+  cout << "]";
+  i++;
+  }
+  cout << endl << "B:";;
+  i=0;
+  while(i<B){
+  cout << "]";
+  i++;
+  }
+  cout << endl;
+}
