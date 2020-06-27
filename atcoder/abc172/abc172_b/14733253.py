5c0498dbcef17d5f34cc1cd896739b414be2fc0f
Balut <hiro.account@gmail.com>
2020-06-27 21:04:30 +0900

    abc172_b

---
+++ atcoder/abc172/abc172_b/14733253.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def main():
+    S = input()
+    T = input()
+    ans = 0
+    for i in range(len(S)):
+        if S[i] != T[i]:
+            ans += 1
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
