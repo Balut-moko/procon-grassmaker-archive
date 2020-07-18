7aee776fd8194cc513b853035c62ccb4355c791b
Balut <hiro.account@gmail.com>
2020-07-18 21:11:23 +0900

    agc039_a

---
+++ atcoder/agc039/agc039_a/15302654.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+def main():
+    S = input()
+    K = int(input())
+
+    cnt = 0
+    ans = 0
+    s_temp = S[0]
+    fcnt = 0
+    for s in S:
+        if s_temp == s:
+            cnt += 1
+            s_temp = s
+        else:
+            ans += cnt // 2
+            if fcnt == 0:
+                fcnt = cnt
+            cnt = 1
+            s_temp = s
+    if fcnt == 0:
+        fcnt = cnt
+    ans += cnt // 2
+    ans *= K
+    if len(S) == fcnt:
+        ans = (len(S) * K) // 2
+    elif S[0] == S[-1] and cnt % 2 == 1 and fcnt % 2 == 1:
+        ans += K - 1
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
