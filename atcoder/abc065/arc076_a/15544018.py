b45458b653e906541135c1f30790a02edbc63682
Balut <hiro.account@gmail.com>
2020-07-31 09:54:37 +0900

    arc076_a

---
+++ atcoder/abc065/arc076_a/15544018.py
@@ -0,0 +1,22 @@
+def factorial(n, mod=10**9 + 7):
+    a = 1
+    for i in range(1, n + 1):
+        a = a * i % mod
+    return a
+
+
+def main():
+    N, M = map(int, input().split())
+    mod = 10 ** 9 + 7
+    if abs(N - M) > 1:
+        ans = 0
+    elif abs(N - M) == 1:
+        ans = (factorial(min(N, M))**2 * max(N, M)) % mod
+    else:
+        ans = (factorial(N)**2 * 2) % mod
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
