24911d42f872e050c4ba8d9999d494cc6f288933
Balut <hiro.account@gmail.com>
2020-09-13 21:06:04 +0900

    abc178_b

---
+++ atcoder/abc178/abc178_b/16682457.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    a, b, c, d = map(int, input().split())
+    ans = max(a * c, a * d, b * c, b * d)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
