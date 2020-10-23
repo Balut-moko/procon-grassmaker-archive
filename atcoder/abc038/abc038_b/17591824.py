85f19c21cc8b37d512e1e3efab009274734ec869
Balut <hiro.account@gmail.com>
2020-10-23 23:39:20 +0900

    abc038_b

---
+++ atcoder/abc038/abc038_b/17591824.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    h1, w1 = map(int, input().split())
+    h2, w2 = map(int, input().split())
+    print('YES' if h1 == h2 or h1 == w2 or w1 == h2 or w1 == w2 else 'NO')
+
+
+if __name__ == "__main__":
+    main()
