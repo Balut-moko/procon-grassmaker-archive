01bfb2bb2ca3bede17e96c2d8108923fd2b5b77e
Balut <hiro.account@gmail.com>
2020-11-21 22:48:09 +0900

    arc108_a

---
+++ atcoder/arc108/arc108_a/18282435.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import itertools
+
+
+def make_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n // i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
+
+def main():
+    s, p = map(int, input().split())
+    p_divisors = make_divisors(p)
+    ans = 'No'
+    for i in range(len(p_divisors) // 2 + 1):
+        if p_divisors[i] + p_divisors[- i - 1] == s:
+            ans = 'Yes'
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
