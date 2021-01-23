3643e1408c5145f4104065860e90a3aa26bddbe2
Balut <hiro.account@gmail.com>
2021-01-23 22:43:53 +0900

    abc189_c

---
+++ atcoder/abc189/abc189_c/19647075.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+    ans = 0
+    for l in range(n):
+        x = a[l]
+        for r in range(l, n):
+            x = min(x, a[r])
+            ans = max(ans, (r - l + 1) * x)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
