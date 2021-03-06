072835a33a323db7d989da95f9d9b046cb89b496
Balut <hiro.account@gmail.com>
2021-03-06 22:49:49 +0900

    abc194_d

---
+++ atcoder/abc194/abc194_d/20732136.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    n = int(input())
+    ans = 0
+    for i in range(1, n):
+        ans += n / i
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
