633c363ede62688261c03f990043f044355f1f5c
Balut <hiro.account@gmail.com>
2021-06-21 00:02:17 +0900

    typical90_p

---
+++ atcoder/typical90/typical90_p/23650426.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+    n = int(input())
+    a, b, c = sorted(map(int, input().split()), reverse=True)
+    ans = 10000
+    for i in range(10000):
+        for j in range(10000 - i):
+            tmp = n - a * i - b * j
+            if 0 <= tmp and tmp % c == 0:
+                ans = min(ans, i + j + tmp // c)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
