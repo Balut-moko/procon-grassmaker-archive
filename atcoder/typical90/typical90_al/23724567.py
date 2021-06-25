9692e69e3e9a7dca1414ba5d767d135e4c0d1087
Balut <hiro.account@gmail.com>
2021-06-25 11:00:26 +0900

    typical90_al

---
+++ atcoder/typical90/typical90_al/23724567.py
@@ -0,0 +1,12 @@
+from sys import stdin
+import math
+
+
+def lcm(x, y):
+    return (x * y) // math.gcd(x, y)
+
+
+readline = stdin.readline
+a, b = map(int, readline().split())
+ans = lcm(a, b)
+print(ans if ans <= 10**18 else 'Large')
