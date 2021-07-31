9b4da867b02e3eda4eede897382c7b3ecb514aca
Balut <hiro.account@gmail.com>
2021-08-01 00:19:57 +0900

    agc040_a

---
+++ atcoder/agc040/agc040_a/24697159.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+readline = stdin.readline
+s = readline()[:-1]
+a = [0] * (len(s) + 1)
+for i in range(len(s)):
+    if s[i] == "<":
+        a[i + 1] = max(a[i] + 1, a[i + 1])
+for i in range(len(s) - 1, -1, -1):
+    if s[i] == ">":
+        a[i] = max(a[i], a[i + 1] + 1)
+print(sum(a))
