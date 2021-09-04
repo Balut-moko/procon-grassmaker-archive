7a1fb4d43341813b120fa7c4f9f97d0073cea471
Balut <hiro.account@gmail.com>
2021-09-05 02:20:07 +0900

    abc217_d

---
+++ atcoder/abc217/abc217_d/25617482.py
@@ -0,0 +1,57 @@
+from sys import stdin
+
+
+class Bit:
+    def __init__(self, n):
+        self.size = n
+        self.tree = [0] * (n + 1)
+        self.depth = n.bit_length()
+
+    def sum(self, i):
+        s = 0
+        while i > 0:
+            s += self.tree[i]
+            i -= i & -i
+        return s
+
+    def add(self, i, x):
+        while i <= self.size:
+            self.tree[i] += x
+            i += i & -i
+
+    def lower_bound(self, x):
+        """ 累積和がx以上になる最小のindexと、その直前までの累積和 """
+        sum_ = 0
+        pos = 0
+        for i in range(self.depth, -1, -1):
+            k = pos + (1 << i)
+            if k <= self.size and sum_ + self.tree[k] < x:
+                sum_ += self.tree[k]
+                pos += 1 << i
+        return pos + 1, sum_
+
+
+readline = stdin.readline
+l, q = map(int, readline().split())
+ans = []
+query = []
+x_set = set()
+for _ in [0] * q:
+    c, x = map(int, readline().split())
+    query.append((c, x))
+    x_set.add(x)
+
+x_li = [0] + sorted(x_set) + [l]
+d = {v: i + 1 for i, v in enumerate(x_li)}
+
+bit = Bit(len(d))
+bit.add(d[0], 1)
+bit.add(d[l], 1)
+for c, x in query:
+    if c == 1:
+        bit.add(d[x], 1)
+    else:
+        idx1, cumsum1 = bit.lower_bound(bit.sum(d[x]) + 1)
+        idx2, cumsum2 = bit.lower_bound(bit.sum(d[x]))
+        ans.append(x_li[idx1 - 1] - x_li[idx2 - 1])
+print(*ans, sep='\n')
