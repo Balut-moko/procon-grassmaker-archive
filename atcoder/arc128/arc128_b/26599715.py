1653bfb554ca950d3b02073c9d899c5bcbd1525a
Balut <hiro.account@gmail.com>
2021-10-16 23:22:13 +0900

    arc128_b

---
+++ atcoder/arc128/arc128_b/26599715.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+
+def solve(rgb):
+    r, g, b = rgb[0], rgb[1], rgb[2]
+    ans = 1 << 30
+    if r == g or g == b:
+        ans = g
+        return ans
+    if (b - r) % 3 == 0:
+        ans = min(ans, b)
+    if (g - r) % 3 == 0:
+        ans = min(ans, g)
+    if (b - g) % 3 == 0:
+        ans = min(ans, b)
+    return ans if ans != (1 << 30) else -1
+
+
+readline = stdin.readline
+t = int(readline())
+for i in range(t):
+    rgb = sorted(map(int, readline().split()))
+    print(solve(rgb))
