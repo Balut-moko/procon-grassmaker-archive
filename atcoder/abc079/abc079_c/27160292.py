c3487e8854838a9352c023754c966373a5ae11fb
Balut <hiro.account@gmail.com>
2021-11-10 13:58:16 +0900

    abc079_c

---
+++ atcoder/abc079/abc079_c/27160292.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+readline = stdin.readline
+s = readline()[:-1]
+for i in range(2**3):
+    tmp = s[0]
+    for j in range(3):
+        if ((i >> j) & 1):
+            tmp += '+' + s[j + 1]
+        else:
+            tmp += '-' + s[j + 1]
+    if eval(tmp) == 7:
+        print(tmp + '=7')
+        exit()
