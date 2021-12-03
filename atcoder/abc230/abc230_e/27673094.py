074819a9286f22d3a5db9679f68b0b98a0cba12b
Balut <hiro.account@gmail.com>
2021-12-03 22:55:57 +0900

    abc230_e

---
+++ atcoder/abc230/abc230_e/27673094.py
@@ -0,0 +1,16 @@
+from sys import stdin
+from math import floor
+readline = stdin.readline
+n = int(readline())
+ans = 0
+for i in range(n + 1):
+    if i * i <= n:
+        k = i
+    else:
+        break
+for i in range(1, k + 1):
+    ans += ((n // i) - (n // (i + 1))) * i
+for i in range(1, n // (k + 1) + 1):
+    ans += n // i
+
+print(ans)
