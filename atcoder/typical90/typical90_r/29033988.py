ac9f9a6bcce292a33cd8b75fd9f8dce5651d9880
Balut <hiro.account@gmail.com>
2022-02-04 11:49:24 +0900

    typical90_r

---
+++ atcoder/typical90/typical90_r/29033988.py
@@ -0,0 +1,20 @@
+from re import I
+from sys import stdin
+import math
+
+
+def calc_angle(a, b):
+    return math.degrees(math.atan2(b, a))
+
+
+readline = stdin.readline
+t = int(readline())
+l, x, y = map(int, readline().split())
+q = int(readline())
+for _ in range(q):
+    e = int(readline())
+    p = math.radians(360 * e / t)
+    y1 = -math.sin(p) * l / 2
+    z1 = l / 2 - math.cos(p) * l / 2
+    a = (x * x + (y - y1) * (y - y1)) ** 0.5
+    print(calc_angle(a, z1))
