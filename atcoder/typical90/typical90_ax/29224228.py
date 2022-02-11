2c3649b0ea55c8d0ec4eb7b04d96fe5bac6a4651
Balut <hiro.account@gmail.com>
2022-02-11 23:55:13 +0900

    typical90_ax

---
+++ atcoder/typical90/typical90_ax/29224228.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+readline = stdin.readline
+n, l = map(int, readline().split())
+mod = 10**9 + 7
+
+dp = [0] * (n + 1)
+dp[0] = 1
+for i in range(1, n + 1):
+    dp[i] += dp[i - 1]
+    if i - l >= 0:
+        dp[i] += dp[i - l]
+    dp[i] %= mod
+print(dp[-1])
