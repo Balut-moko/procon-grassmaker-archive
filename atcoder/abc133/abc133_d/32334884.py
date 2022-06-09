fb65b4666112d30411c06719d6389834ed8fb621
Balut <hiro.account@gmail.com>
2022-06-09 17:54:12 +0900

    abc133_d

---
+++ atcoder/abc133/abc133_d/32334884.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+readline = stdin.readline
+n = int(readline())
+a = list(map(int, readline().split()))
+ans = [0] * n
+
+for i in range(n):
+    if i % 2 == 0:
+        ans[0] += a[i]
+    else:
+        ans[0] -= a[i]
+
+for i in range(1, n):
+    ans[i] = 2 * a[i - 1] - ans[i - 1]
+
+print(*ans)
