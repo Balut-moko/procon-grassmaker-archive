d8099b340cb53285fd7bcedf0f091a0301634c1b
Balut <hiro.account@gmail.com>
2022-06-13 18:21:46 +0900

    agc016_a

---
+++ atcoder/agc016/agc016_a/32453056.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from sys import stdin
+
+readline = stdin.readline
+s = readline()[:-1]
+ans = 100
+for i in range(26):
+    al = chr(ord("a") + i)
+    cnt = 0
+    ss = s
+    while ss != al * len(ss):
+        tmp = ""
+        for i in range(len(ss) - 1):
+            if ss[i] == al or ss[i + 1] == al:
+                tmp += al
+            else:
+                tmp += ss[i]
+        ss = tmp
+        cnt += 1
+    ans = min(ans, cnt)
+print(ans)
