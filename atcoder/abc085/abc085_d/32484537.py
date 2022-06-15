a602355d67296bad8f78e47f4c99bc9e23a68f3a
Balut <hiro.account@gmail.com>
2022-06-15 17:20:44 +0900

    abc085_d

---
+++ atcoder/abc085/abc085_d/32484537.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+readline = stdin.readline
+n, h = map(int, readline().split())
+ab = [tuple(map(int, readline().split())) for _ in [0] * n]
+
+ab = sorted(ab, reverse=True, key=lambda x: x[1])
+a_max = max([val[0] for val in ab])
+
+ans = 0
+for i, val in enumerate(ab):
+    if a_max <= val[1]:
+        h -= val[1]
+        ans += 1
+        if h <= 0:
+            print(ans)
+            exit()
+
+
+div, mod = divmod(h, a_max)
+ans += div
+if mod != 0:
+    ans += 1
+print(ans)
