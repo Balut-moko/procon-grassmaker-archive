f640012d425c3686da26a8c635482abbc49ce64a
Balut <hiro.account@gmail.com>
2022-08-09 18:14:32 +0900

    agc011_b

---
+++ atcoder/agc011/agc011_b/33898494.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+readline = stdin.readline
+n = int(readline())
+a = list(map(int, readline().split()))
+a.sort()
+cnt = 0
+t = 0
+for i in range(n - 1):
+    cnt += a[i]
+    if cnt * 2 < a[i + 1]:
+        t = i + 1
+print(n - t)
