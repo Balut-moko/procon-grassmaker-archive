2649dea3b54086fda8859be21479ac179e2b55b0
Balut <hiro.account@gmail.com>
2023-01-30 17:07:37 +0900

    abc287_c

---
+++ atcoder/abc287/abc287_c/38480322.py
@@ -0,0 +1,36 @@
+from sys import stdin
+from collections import deque
+
+
+def bfs(graph):
+    dist = [False] * len(graph)
+    que = deque([[0, -1]])  # root
+    dist[0] = True
+    flag = True
+    while que:
+        v, pre = que.popleft()
+        if len(graph[v]) > 2:
+            flag = False
+        for w in graph[v]:
+            if pre == w:
+                continue
+            if dist[w]:
+                flag = False
+                continue
+            dist[w] = True
+            que.append([w, v])
+    if not all(dist):
+        flag = False
+    return flag
+
+
+readline = stdin.readline
+n, m = map(int, readline().split())
+
+graph = [[] for _ in range(n)]
+for _ in range(m):
+    a, b = map(lambda x: int(x) - 1, readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print("Yes" if bfs(graph) else "No")
