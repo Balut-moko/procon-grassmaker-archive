bb118a61b65f4533fa0a45c36d3097397fd35bd9
Balut <hiro.account@gmail.com>
2023-05-26 09:19:42 +0900

    arc074_a

---
+++ atcoder/abc062/arc074_a/41695587.py
@@ -0,0 +1,47 @@
+from sys import stdin
+
+readline = stdin.readline
+H, W = map(int, readline().split())
+
+
+def cut_rectangle(h, w):
+    mex_h = h // 2
+    sh1 = mex_h * w
+    sh2 = (h - mex_h) * w
+    if sh2 < sh1:
+        sh1, sh2 = sh2, sh1
+    return sh1, sh2
+
+
+ans = 1 << 60
+for h in range(1, H):
+    h1 = H - h
+    h2 = h
+    s1 = h1 * W
+    s2 = h2 * W
+    if s2 < s1:
+        s1, s2 = s2, s1
+        h1, h2 = h2, h1
+    s3, s4 = cut_rectangle(h2, W)
+    s5, s6 = cut_rectangle(W, h2)
+    if s6 - s5 < s4 - s3:
+        s3, s4 = s5, s6
+    tmp = max(s1, s3, s4) - min(s1, s3, s4)
+    ans = min(ans, tmp)
+for w in range(1, W):
+    w1 = W - w
+    w2 = w
+    s1 = w1 * H
+    s2 = w2 * H
+    if s2 < s1:
+        s1, s2 = s2, s1
+        w1, w2 = w2, w1
+
+    s3, s4 = cut_rectangle(w2, H)
+    s5, s6 = cut_rectangle(H, w2)
+    if s6 - s5 < s4 - s3:
+        s3, s4 = s5, s6
+    tmp = max(s1, s3, s4) - min(s1, s3, s4)
+    ans = min(ans, tmp)
+
+print(ans)
