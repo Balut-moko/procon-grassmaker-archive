efb8b9d72eee07c015c5ab56a8eadc17ee4be556
Balut <hiro.account@gmail.com>
2023-09-18 23:28:51 +0900

    abc040_d

---
+++ atcoder/abc040/abc040_d/45712790.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+from atcoder.dsu import DSU
+
+
+n, m = map(int, input().split())
+aby = [tuple(map(int, input().split())) for _ in [0] * m]
+
+q = int(input())
+vw = [tuple([i] + list(map(int, input().split()))) for i in range(q)]
+
+aby.sort(reverse=True, key=lambda x: x[2])
+vw.sort(reverse=True, key=lambda x: x[2])
+que1 = deque(aby)
+que2 = deque(vw)
+
+uf = DSU(n + 1)
+ans = [None] * q
+while que2:
+    i, v, w = que2.popleft()
+    while que1:
+        a, b, y = que1.popleft()
+        if w < y:
+            uf.merge(a, b)
+        else:
+            que1.appendleft((a, b, y))
+            break
+    ans[i] = uf.size(v)
+
+print(*ans, sep="\n")
