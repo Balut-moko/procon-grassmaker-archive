4d7ca15b9957d3554279eba9f84ecbddcad7a7ff
Balut <hiro.account@gmail.com>
2024-01-19 23:00:59 +0900

    dp_a

---
+++ atcoder/dp/dp_a/49414467.py
@@ -0,0 +1,13 @@
+N = int(input())
+H = list(map(int, input().split()))
+
+dp = [1 << 60] * N
+dp[0] = 0
+dp[1] = abs(H[0] - H[1])
+
+for i in range(2, N):
+    dp[i] = min(
+        dp[i], dp[i - 1] + abs(H[i - 1] - H[i]), dp[i - 2] + abs(H[i - 2] - H[i])
+    )
+
+print(dp[-1])
