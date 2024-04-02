e997a14a621eb7d1191d550706f3d993624fec9e
Balut <hiro.account@gmail.com>
2024-04-02 12:25:54 +0900

    typical90_bj

---
+++ atcoder/typical90/typical90_bj/51954754.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+
+N = int(input())
+AB = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in [0] * N]
+
+que = deque([])
+white = [False] * N
+ans = []
+graph = [[] for _ in range(N)]
+for i in range(N):
+    a, b = AB[i]
+    graph[a].append(i)
+    graph[b].append(i)
+    if a == i or b == i:
+        que.append(i)
+
+while que:
+    v = que.popleft()
+    if not white[v]:
+        ans.append(v + 1)
+    white[v] = True
+    for w in graph[v]:
+        if white[w]:
+            continue
+        que.append(w)
+
+if len(ans) == N:
+    print(*ans[::-1], sep="\n")
+else:
+    print(-1)
